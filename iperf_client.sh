26eb1c6b63a2a96515fb51eaf6fdc263c2f77d99
Leo <leoshch@gmail.com>
2018-11-30 08:26:19 +0000

    Update iperf_client.sh

--- iperf_client.sh
+++ iperf_client.sh
@@ -4,11 +4,11 @@ stream=$2
 port=$3
 ip=$4
 for(( i=0; i <$stream; i++))
-do 
-    if [ "$type"=="udp" ];then
+do
+    if [ "$type" = "udp" ];then
         iperf3 -u -c $ip -p $port -l 64 -b 0 -t 0 -i 0 &
         echo "iperf3 -u -c $ip -p $port -l 64 -b 0 -t 0 -i 0 &"
-    elif [ "$type"=="tcp" ];then
+    elif [ "$type" = "tcp" ];then
         iperf3 -c $ip -p $port -t 0 -i 0 &
         echo "iperf3 -c $ip -p $port -t 0 -i 0"
     fi
