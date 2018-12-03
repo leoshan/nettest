cf1a466e9301b4d937b630e3a64a8fc17c8915ec
Leo <leoshch@gmail.com>
2018-12-03 11:25:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@ yum install -y gcc automake iperf3
 git clone https://github.com/linux-rdma/qperf.git
 git clone https://github.com/linux-rdma/qperf.git
 git clone https://github.com/esnet/iperf.git
-cd ./qperf/src
+cd ./qperf
 ./cleanup
 ./autogen.sh
 ./configure
-make
+make install
 cd ../../iperf
 ./configure
 make
 make install
+cp /usr/local/bin/iperf3 /usr/bin
+cp /usr/local/bin/qperf /usr/bin
